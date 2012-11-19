f90d148b33e5aae9ca5636520bd904cc6b3b7040
Robert Shannon <spirit2457@gmail.com>
2012-11-19 07:24:34 -0700

    new file:   AndroidBoard.mk 	new file:   AndroidProducts.mk 	new file:   BoardConfig.mk 	new file:   cm.mk 	new file:   device_evitareul.mk 	new file:   kernel 	new file:   prebuilt/kernel 	new file:   recovery.fstab 	new file:   recovery/init 	new file:   recovery/init.rc 	new file:   recovery/init.twrp.rc 	new file:   recovery/sbin/adbd 	new file:   recovery/sbin/choice_fn 	new file:   recovery/sbin/detect_key 	new file:   recovery/sbin/gzip_recvy 	new file:   recovery/sbin/htc_ebdlogd_recvy 	new file:   recovery/sbin/htcbatt 	new file:   recovery/sbin/logcat2_recvy 	new file:   recovery/sbin/mfgsrv 	new file:   recovery/sbin/offmode_charging 	new file:   recovery/sbin/postrecoveryboot.sh 	new file:   recovery/sbin/power_test 	new file:   recovery/sbin/recovery 	new file:   recovery/sbin/sfc 	new file:   recovery/sbin/ueventd 	new file:   system.prop 	new file:   vendorsetup.sh

---
+++ BoardConfig.mk
@@ -0,0 +1,110 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/htc/evitareul/BoardConfigVendor.mk
+
+# Skip droiddoc build to save build time
+BOARD_SKIP_ANDROID_DOC_BUILD := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false
+
+#Camera
+USE_CAMERA_STUB := false
+CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
+BOARD_HAVE_HTC_FFC := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK 
+COMMON_GLOBAL_CPPFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := tegra
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x
+BOARD_KERNEL_PAGESIZE := 
+
+# Board naming
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := 
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := t30
+
+# EGL settings
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/htc/evitareul/configs/egl.cfg
+
+# BT
+BOARD_HAVE_BLUETOOTH := true
+
+# HTC ril compatability
+BOARD_USE_NEW_LIBRIL_HTC := true
+TARGET_PROVIDES_LIBRIL := vendor/htc/evitareul/proprietary/lib/libhtc-ril.so
+
+# Avoid the generation of ldrcc instructions
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+
+ # Connectivity - Wi-Fi
+USES_TI_MAC80211 := true
+ifdef USES_TI_MAC80211
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER             := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+endif
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/htc/evitareul/kernel
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Vold / USB
+BOARD_VOLD_MAX_PARTITIONS := 20
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
+#CWM Recovery UNCOMMENT TO USE CWM
+#TARGET_RECOVERY_INITRC := device/htc/evitareul/recovery/init.rc
+#BOARD_USES_MMCUTILS := true
+#BOARD_HAS_NO_MISC_PARTITION := false
+#BOARD_UMS_LUNFILE := "/sys/devices/platform/fsl-tegra-udc/gadget/lun0/file"
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/evitareul/prebuilt/recovery_kernel
+
+# TWRP Configurations comment these out to use CWM
+DEVICE_RESOLUTION := 720x1280
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_RECOVERY_INITRC := device/htc/evitareul/recovery/init.twrp.rc
+TW_FLASH_FROM_STORAGE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+#TW_INCLUDE_CRYPTO := true
+#TW_CRYPTO_FS_TYPE := "ext4"
+#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p14"
+#TW_CRYPTO_MNT_POINT := "/data"
+#TW_CRYPTO_FS_OPTIONS := "barrier=0"
+#TW_CRYPTO_FS_FLAGS := "0x00000406"
+#TW_CRYPTO_KEY_LOC := "footer"
